c5f426f772c75a3e56bac3ce222521881de2f1db
Raul Silveira <raulantsilveira@gmail.com>
2024-10-14 13:33:50 -0300

    Avatar component created

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/raul4nt.png"/>
+                    <Avatar src="https://github.com/Lucas-Gades.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Raul Silveira</strong>
                         <span>Estudante de TI</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from 'phosphor-react'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/raul4nt.png"/>
+                <Avatar src="https://github.com/raul4nt.png"/>
 
 
                 <strong>Raul Silveira</strong>
