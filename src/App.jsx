f08ea1050f88c5491dc40448feb21ba7938b1593
Raul Silveira <raulantsilveira@gmail.com>
2024-10-14 14:34:44 -0300

    iterating in react

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,38 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+  id: 1,
+  author: {
+    avatarUrl: 'http://github.com/raul4nt.png',
+    name: 'Raul Silveira',
+    role: 'Student @Rocketseat'
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋' },
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+    { type: 'link', content: 'jane.design/doctorcare' },
+  ],
+  publishedAt: new Date('2024-10-14 11:32:00'),
+},
+{
+  id: 2,
+  author: {
+    avatarUrl: 'http://github.com/Lucas-Gades.png',
+    name: 'Lucas Gades',
+    role: 'Estagiário de TI'
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋' },
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+    { type: 'link', content: 'jane.design/doctorcare' },
+  ],
+  publishedAt: new Date('2024-10-13 15:51:00'),
+},
+]
+
+
 export function App() {
   return (
     <div>
@@ -15,20 +47,19 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />  
           <main>
-            <Post 
-              author="Raul Silveira"
-              content="Concordo linda solteira"
-            />
-
-            <Post 
-              author="Rafael Gastão"
-              content="Não tem chupix "
-            />
-
+            {posts.map(post => {
+              return (<Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+            })}
           </main>
       </div> 
     </div>
   )
 }
 
-
+// sempre que for feito uma iteração dentro do React, usaremos o Map
+// o forEach nao pode ser usado pois nao tem retorno, ja o map tem
