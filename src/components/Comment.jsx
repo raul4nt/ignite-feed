4ab7321f4bd49414cdcf399963a5bdaaed454b1b
Raul Silveira <raulantsilveira@gmail.com>
2024-11-05 22:36:50 -0300

    feat: create handleDeleteComment function

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/cferrugem.png"/>
@@ -15,12 +20,12 @@ export function Comment() {
                             <time title='12 de Setembro às 19:12h' dateTime="2024-10-12 19:12:25">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
 
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
             <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,13 +11,14 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }){
     const [comments, setComments] = useState([
-        1,
-        2,
+       'Post muito bacana, hein?'
         // o UseState do react é interssante usarmos destruturaçao. ele recebe primeiro
         // o valor q ele possuirá de começo, e depois uma funçao(que escolhemos o nome setComments),
         // que vai ser a funçao usada pra adicionarmos algo no valor default.
         // ou seja, manipularemos os comments atraves de setComments.
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
        locale: ptBR,
@@ -37,8 +38,8 @@ export function Post({ author, publishedAt, content }){
         // como default. neste caso, quando usamos o onSubmit em html, o default
         // é, apos clicar no botao de submit ou apertar enter, ele nos redirecionar pra 
         // outra pagina. mas nao é o que queremos, por isso, usamos o preventDefault.
-        
-        setComments([...comments, comments.length + 1]);
+        // const newCommentText = event.target.comment.value
+        setComments([...comments, newCommentText]);
         // ... = spread operator. copia o que ja existe. neste caso, tem 1, 2 lá
         // na variável comments(array). entao ele pega tudo isso, e ai adiciona mais um
         // pois estamos usando o comments.lenght + 1. se em comments tem 1 e 2, a lenght é 
@@ -46,7 +47,21 @@ export function Post({ author, publishedAt, content }){
         // nesse "setador" do UseState do react sempre devemos passar o novo valor q ele receberá,
         // e nao apenas um acrescimo. por isso, precisamos recuperar o valor antigo(uasndo o ...comments),
         // e ai sim acrescentar algo.
-        
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete
+        }) 
+        // gerar uma nova lista deixando apenas os comentarios que sejam diferentes do comentario
+        // que eu quero deletar
+
+        setComments(commentsWithoutDeletedOne)
     }
 
 
@@ -69,9 +84,9 @@ export function Post({ author, publishedAt, content }){
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -79,8 +94,12 @@ export function Post({ author, publishedAt, content }){
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea 
+                <textarea
+                    name="comment"
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    // monitora toda vez que alterar o conteudo
                 />
 
                 <footer>
@@ -90,7 +109,7 @@ export function Post({ author, publishedAt, content }){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
                 })}
             </div>
 
